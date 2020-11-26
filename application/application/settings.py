77c7ae40e7cd023d1371eb19031244e8add15b53
Kyle <kbburkholder@gmail.com>
2020-11-25 19:52:15 -0500

    Making the secrety key an env var, the old key was never used in production.

--- application/application/settings.py
+++ application/application/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fwt!ocv)n3zg3nuv1nzp#bz5u@z!1&&@0#8+3w$^ins6u1#q)s'
+SECRET_KEY = os.environ['DJANGO-SECRET-KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
