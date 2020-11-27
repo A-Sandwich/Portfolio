63189bcf76c1b5a1d77785bde970ce893a549679
Kyle <kbburkholder@gmail.com>
2020-11-27 15:37:08 -0500

    Fixing environment var issues

--- application/todo/views.py
+++ application/todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return HttpResponse("✨This is the index✨")
+    tasks = Task.objects.filter(completed=False)
+    output = ', '.join([t.title for t in tasks])
+    return HttpResponse(output)
