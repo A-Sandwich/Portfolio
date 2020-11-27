150f0cdb758be02756057275fb5be9950f8c047f
Kyle <kbburkholder@gmail.com>
2020-11-27 16:59:33 -0500

    Started adding a form to create new tasks

--- application/todo/urls.py
+++ application/todo/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
 from . import views
 
+# https://docs.djangoproject.com/en/3.1/intro/tutorial03/#namespacing-url-names
+app_name = 'todo'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('save/', views.save, name='save'),
 ]
--- application/todo/views.py
+++ application/todo/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Task
 
 def index(request):
     tasks = Task.objects.filter(completed=False)
     output = ', '.join([t.title for t in tasks])
     return HttpResponse(output)
+
+def create(request):
+    return render(request, 'todo/create.html', {})
+
+def save(request):
+    print(request.POST['title'])
+    return HttpResponseRedirect(reverse('todo:create'))
